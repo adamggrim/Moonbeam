94c5ea86adc75a4d931e8501308655900b23ab39
adamggrim <97310482+adamggrim@users.noreply.github.com>
2024-07-17 22:38:23 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,20 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Moonbeam"
+    name: "Moonbeam",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "Moonbeam",
+            targets: ["Moonbeam"]),
+    ],
+    targets: [
+        .target(
+            name: "Moonbeam",
+            dependencies: []),
+    ]
 )
