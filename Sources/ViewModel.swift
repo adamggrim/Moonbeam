f78d6f7bacb1fb54af4f4951b710242b08dcedbf
adamggrim <97310482+adamggrim@users.noreply.github.com>
2024-07-22 15:39:25 -0400

    Create ViewModel.swift

---
+++ Sources/ViewModel.swift
@@ -0,0 +1,91 @@
+import Foundation
+
+import SwiftUI
+import Observation
+
+@Observable
+class ColorSliderViewModel {
+        
+    // Drag variable to get the color of the color preview (different from persistedDrag whenever the drag extends to the width of the thumb at the end of the slider)
+    var colorDrag: CGFloat = .zero
+    var isDragging: Bool = false
+    
+    var persistedDrag: CGFloat = .zero
+    var sliderDrag: CGFloat = .zero
+    var containerDrag: CGFloat = .zero
+    var newDrag: CGFloat = .zero
+    
+    var color: Color
+    var thumbColor: Color
+    let dimensions: ColorSliderDimensions
+    
+    init(color: Color, thumbColor: Color, dimensions: ColorSliderDimensions) {
+        self.color = color
+        self.thumbColor = thumbColor
+        self.dimensions = dimensions
+        
+        let uiColor = UIColor(color)
+        var hue: CGFloat = 0
+        
+        uiColor.getHue(&hue, saturation: nil, brightness: nil, alpha: nil)
+        let dragValue = calculateDrag(hue: hue, sliderWidth: dimensions.sliderWidth)
+        
+        self.persistedDrag = dragValue
+        self.sliderDrag = dragValue
+        self.containerDrag = dragValue
+        self.newDrag = dragValue
+    }
+    
+    var calculatedColor: Color {
+        let colorIndex = min(sliderColors.count - 1, Int(CGFloat(sliderColors.count) * (colorDrag / dimensions.sliderWidth)))
+        return sliderColors[colorIndex]
+    }
+    
+    var colorPreviewHorizontalOffset: CGFloat {
+        let halfColorPreviewWidth = dimensions.colorPreviewWidth / 2
+        let halfThumbWidth = dimensions.thumbWidth / 2
+        
+        let leftBound = halfColorPreviewWidth - halfThumbWidth
+        let rightBound = dimensions.sliderWidth - halfColorPreviewWidth - halfThumbWidth
+        
+        // Clamp the persistedDrag value within the left and right bounds.
+        let clampedValue = min(max(persistedDrag, leftBound), rightBound)
+        
+        let effectiveOffset = thumbOffset + halfThumbWidth
+
+            if !isDragging && effectiveOffset < halfColorPreviewWidth {
+                return -halfColorPreviewWidth + effectiveOffset
+            } else if !isDragging && effectiveOffset > dimensions.sliderWidth - halfColorPreviewWidth {
+                return dimensions.sliderWidth - halfColorPreviewWidth - halfThumbWidth
+            }
+
+            return clampedValue - halfColorPreviewWidth + halfThumbWidth
+    }
+    
+    var colorPreviewVerticalOffset: CGFloat {
+        return -dimensions.sliderHeight * 3.3333
+    }
+    
+    var thumbOffset: CGFloat {
+        let leftBound = 0.0
+        let rightBound = dimensions.sliderWidth - dimensions.thumbWidth
+        
+        // Clamp the persistedDrag value within the left and right bounds.
+        return min(max(persistedDrag, leftBound), rightBound)
+    }
+    
+    func onDragChanged(_ value: DragGesture.Value) {
+        
+        containerDrag = value.translation.width
+        newDrag = sliderDrag + containerDrag
+        
+        // Clamp to prevent the drag gesture from displacing the thumb on rebound from the left and right edges of the slider.
+        colorDrag = min(max(newDrag, 0), dimensions.sliderWidth)
+        persistedDrag = min(max(newDrag, 0), dimensions.sliderWidth - dimensions.thumbWidth)
+    }
+    
+    func onDragEnded() {
+        sliderDrag = persistedDrag
+        containerDrag = .zero
+    }
+}
