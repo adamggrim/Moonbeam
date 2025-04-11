13e102178a53c8317528fa9e2e52bd33dbc2b95e
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-04-10 23:55:03 -0400

    Create constants for RGBColorSliderModel

--- Sources/Model/RGB Model/ColorSlider.swift
+++ Sources/Model/RGB Model/ColorSlider.swift
@@ -3,4 +3,13 @@ import SwiftUI
 
 // Model for assembling the RGB color slider values into an array
 struct RGBColorSliderModel {
+    let startRed: Double
+    let startGreen: Double
+    let startBlue: Double
+
+    let endRed: Double
+    let endGreen: Double
+    let endBlue: Double
+    
+    let count: Int
 }
