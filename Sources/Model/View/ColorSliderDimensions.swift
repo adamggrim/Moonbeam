1d660779036ebb751c27faf3f90811ea3f5738e7
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-05-10 16:04:33 -0400

    Move ColorSliderDimensions to separate file

---
+++ Sources/Model/View/ColorSliderDimensions.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+struct ColorSliderDimensions {
+    let sliderWidth: CGFloat
+    let sliderHeight: CGFloat
+    let thumbWidth: CGFloat
+    let thumbHeight: CGFloat?
+    let previewWidth: CGFloat
+    let previewCornerRadius: CGFloat
+    let previewOffset: CGFloat
+    let shadowRadius: CGFloat
+    let scaleRatio: CGFloat = 0.25
+    
+    init(
+        sliderWidth: CGFloat,
+        sliderHeight: CGFloat,
+        thumbWidth: CGFloat? = nil,
+        thumbHeight: CGFloat? = nil,
+        previewWidth: CGFloat,
+        previewOffset: CGFloat,
+        shadowRadius: CGFloat
+    ) {
+        self.sliderWidth = sliderWidth
+        self.sliderHeight = sliderHeight
+        self.thumbWidth = thumbWidth ?? sliderHeight
+        self.thumbHeight = thumbHeight ?? sliderHeight * 2 // Ignored when thumbStyle is .circle
+        self.previewWidth = previewWidth
+        self.previewOffset = previewOffset
+        self.shadowRadius = shadowRadius
+        self.previewCornerRadius = previewWidth * 0.225
+    }
+}
