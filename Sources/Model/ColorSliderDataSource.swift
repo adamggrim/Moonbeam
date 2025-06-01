0608fbf670361b1eacaaa11f0124d4b85cc4eeda
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-06-01 10:01:32 -0400

    Create ColorSourceProvider enum

--- Sources/Model/ColorSliderDataSource.swift
+++ Sources/Model/ColorSliderDataSource.swift
@@ -1,6 +1,23 @@
 import SwiftUI
 
+enum ColorSourceProvider {
+    /**
+     Provides colors as a pre-computed array.
+     
+     Used for hard-edge colors.
+     */
+    case precomputed([Color])
+    
+    /**
+     Dynamically calculates a color based on normalized position (0.0 to 1.0).
+    
+     Used for spectrums and gradients.
+     */
+    case dynamic((_ position: Double) -> Color)
+}
+
 /// Protocol shared by `SpectrumSliderModel` and `GradientSliderModel`.
 protocol ColorSliderDataSource {
-    var sliderColors: [Color] { get }
+    var colorSource: ColorSourceProvider { get }
+
 }
