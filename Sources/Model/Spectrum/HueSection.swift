b074049efbf465361601dffdced92512b48f86e3
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-05-02 14:09:56 -0400

    Update and rename bend sections

--- Sources/Model/Spectrum/HueSection.swift
+++ Sources/Model/Spectrum/HueSection.swift
@@ -40,77 +40,37 @@ public struct HueSection {
 }
 
 /// A bend section that fades into the start or end of a color slider.
-struct OneWayBendSection: BendSection {
-    let startHue: CGFloat
-    let endHue: CGFloat
-    
-    let targetSaturation: CGFloat
-    let baseSaturation: CGFloat
-    let saturationIncrement: CGFloat
-    
-    let targetBrightness: CGFloat
-    let baseBrightness: CGFloat
-    let brightnessIncrement: CGFloat
-    
-    let hueCount: CGFloat
-    
-    init(
-        startHue: CGFloat,
-        endHue: CGFloat,
-        targetSaturation: CGFloat,
-        baseSaturation: CGFloat,
-        targetBrightness: CGFloat,
-        baseBrightness: CGFloat
-    ) {
-        let hueCount = endHue - startHue
-        let saturationDelta = baseSaturation - targetSaturation
-        let brightnessDelta = baseBrightness - targetBrightness
-        
-        self.startHue = startHue
-        self.endHue = endHue
-        
-        self.targetSaturation = targetSaturation
-        self.baseSaturation = baseSaturation
-        self.saturationIncrement = hueCount != 0 ? saturationDelta / hueCount : 0
-        
-        self.targetBrightness = targetBrightness
-        self.baseBrightness = baseBrightness
-        self.brightnessIncrement = hueCount != 0 ? brightnessDelta / hueCount : 0
-        
-        self.hueCount = hueCount
+public struct OneWayBend: BendSection {
+    public let startHue: CGFloat
+    public let endHue: CGFloat
+    public let targetSaturation: CGFloat
+    public let targetBrightness: CGFloat
+    public let hueCount: CGFloat
+
+    public init(hue range: ClosedRange<CGFloat>, saturation: CGFloat = 1.0, brightness: CGFloat = 1.0) {
+        self.startHue = range.lowerBound
+        self.endHue = range.upperBound
+        self.targetSaturation = saturation
+        self.targetBrightness = brightness
+        self.hueCount = range.upperBound - range.lowerBound
     }
 }
 
 /// A bend section that occurs in the middle of a color slider.
-struct TwoWayBendSection: BendSection {
-    let startHue: CGFloat
-    let endHue: CGFloat
-    
-    let targetSaturation: CGFloat
-    let baseSaturation: CGFloat
-    
-    let targetBrightness: CGFloat
-    let baseBrightness: CGFloat
-    
-    let hueCount: CGFloat
-    
-    init(
-        startHue: CGFloat,
-        endHue: CGFloat,
-        targetSaturation: CGFloat,
-        baseSaturation: CGFloat,
-        targetBrightness: CGFloat,
-        baseBrightness: CGFloat
-    ) {
-        self.startHue = startHue
-        self.endHue = endHue
-        
-        self.targetSaturation = targetSaturation
-        self.baseSaturation = baseSaturation
-        
-        self.targetBrightness = targetBrightness
-        self.baseBrightness = baseBrightness
-        
-        self.hueCount = endHue - startHue
+public struct TwoWayBend: BendSection {
+    public let startHue: CGFloat
+    public let endHue: CGFloat
+    public let targetSaturation: CGFloat
+    public let targetBrightness: CGFloat
+    public let hueCount: CGFloat
+    public let middleHue: CGFloat
+
+    public init(hue range: ClosedRange<CGFloat>, saturation: CGFloat = 1.0, brightness: CGFloat = 1.0) {
+        self.startHue = range.lowerBound
+        self.endHue = range.upperBound
+        self.targetSaturation = saturation
+        self.targetBrightness = brightness
+        self.hueCount = range.upperBound - range.lowerBound
+        self.middleHue = (self.startHue + self.hueCount) / 2
     }
 }
