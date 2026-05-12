d9772bc8b2e7dc06ea405a5a22d4f7d18c1a8fbb
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-05-12 01:24:58 -0400

    Add documentation for BendSectionBuilder and SpectrumComponentBuilder

--- Sources/Models/Spectrum/SpectrumBuilders.swift
+++ Sources/Models/Spectrum/SpectrumBuilders.swift
@@ -1,6 +1,7 @@
 import Foundation
 import SwiftUI
 
+/// A result builder used to construct an array of `BendSection` components.
 @resultBuilder
 public struct BendSectionBuilder {
     public static func buildBlock(_ components: BendSection...) -> [BendSection] { return Array(components) }
@@ -9,6 +10,7 @@ public struct BendSectionBuilder {
     public static func buildEither(second component: [BendSection]) -> [BendSection] { return component }
 }
 
+/// A result builder used to construct a spectrum slider using `SliderComponent` blocks.
 @resultBuilder
 public struct SpectrumComponentBuilder {
     public static func buildBlock(_ components: SliderComponent...) -> [SliderComponent] { return Array(components) }
