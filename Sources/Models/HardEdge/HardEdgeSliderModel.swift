3f3e8dbb398d6a4dbbfa988c112ba3dda2b51a9e
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-05-13 12:19:23 -0400

    Create HardEdgeSliderModel.swift

---
+++ Sources/Models/HardEdge/HardEdgeSliderModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+import SwiftUI
+
+/// Model for a slider with discrete color blocks.
+///
+///Supports explicit and implicit hard-edge sliders:
+/// 1. **Explicit**: Initialize the slider using a custom array of `Color` objects.
+/// 2. **Implicit**: Apply the `.hardEdge(into:)` modifier to convert a `SpectrumSliderModel` or `GradientSliderModel` into discrete color blocks.
+public struct HardEdgeSliderModel: ColorSliderDataSource {
+    public let colors: [Color]
+    
+    public var colorSource: ColorSourceProvider {
+        return .array(colors)
+    }
+    
+    public init(colors: [Color]) {
+        self.colors = colors
+    }
+}
