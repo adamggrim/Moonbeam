caa147e6465eca811ba0533265e0b0a299abfe59
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-07-08 02:54:38 -0400

    Create ColorSliderDefaults.swift

---
+++ Sources/Moonbeam/Environment/ColorSliderDefaults.swift
@@ -0,0 +1,11 @@
+import CoreGraphics
+
+/// Global default metrics for  color sliders.
+public enum ColorSliderDefaults {
+    public static let trackLength: CGFloat = 300.0
+    public static let trackThickness: CGFloat = 24.0
+    public static let previewSize: CGFloat = 60.0
+    public static let previewOffset: CGFloat = 70.0
+    public static let dragScaleMultiplier: CGFloat = 1.1
+    public static let accessibilityStepPercentage: Double = 0.05
+}
