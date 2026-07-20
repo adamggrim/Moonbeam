3590bc2e5c7d2c2384fedbaf308680d7483d2e40
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-07-20 00:56:35 -0400

    Move `BendSectionBuilder` to SpectrumComponents.swift

--- Sources/Moonbeam/Models/Spectrum/SpectrumComponents.swift
+++ Sources/Moonbeam/Models/Spectrum/SpectrumComponents.swift
@@ -50,6 +50,15 @@ public extension BendSection {
     }
 }
 
+/// A result builder used to construct an array of `BendSection` components.
+@resultBuilder
+public struct BendSectionBuilder {
+    public static func buildBlock(_ components: BendSection...) -> [BendSection] { return Array(components) }
+    public static func buildOptional(_ component: [BendSection]?) -> [BendSection] { return component ?? [] }
+    public static func buildEither(first component: [BendSection]) -> [BendSection] { return component }
+    public static func buildEither(second component: [BendSection]) -> [BendSection] { return component }
+}
+
 // MARK: - Monochrome sections
 
 /// A spectrum section that resolves to pure black.
