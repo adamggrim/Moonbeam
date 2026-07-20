bcdca8cbd7dea2871131a061c1e55471449358e5
adamggrim <97310482+adamggrim@users.noreply.github.com>
2026-07-19 22:06:16 -0400

    Update constant naming in ColorSpaceConverter.swift

--- Sources/Moonbeam/Models/ColorSpaceConverter.swift
+++ Sources/Moonbeam/Models/ColorSpaceConverter.swift
@@ -15,17 +15,17 @@ internal enum ColorSpaceConverter {
         let a = chroma * cos(hueAngle)
         let b = chroma * sin(hueAngle)
 
-        let lightnessPrime = lightness + 0.3963377774 * a + 0.2158037573 * b
+        let longPrime = lightness + 0.3963377774 * a + 0.2158037573 * b
         let mediumPrime = lightness - 0.1055613458 * a - 0.0638541728 * b
-        let smallPrime = lightness - 0.0894841775 * a - 1.2914855480 * b
+        let shortPrime = lightness - 0.0894841775 * a - 1.2914855480 * b
 
-        let lightnessCubed = lightnessPrime < 0 ? -pow(-lightnessPrime, 3.0) : pow(lightnessPrime, 3.0)
+        let longCubed = longPrime < 0 ? -pow(-longPrime, 3.0) : pow(longPrime, 3.0)
         let mediumCubed = mediumPrime < 0 ? -pow(-mediumPrime, 3.0) : pow(mediumPrime, 3.0)
-        let smallCubed = smallPrime < 0 ? -pow(-smallPrime, 3.0) : pow(smallPrime, 3.0)
+        let shortCubed = shortPrime < 0 ? -pow(-shortPrime, 3.0) : pow(shortPrime, 3.0)
 
-        let redLinear =   4.0767416621 * lightnessCubed - 3.3077115913 * mediumCubed + 0.2309699292 * smallCubed
-        let greenLinear = -1.2684380046 * lightnessCubed + 2.6097574011 * mediumCubed - 0.3413193965 * smallCubed
-        let blueLinear = -0.0041960863 * lightnessCubed - 0.7034186147 * mediumCubed + 1.7076147010 * smallCubed
+        let redLinear =   4.0767416621 * longCubed - 3.3077115913 * mediumCubed + 0.2309699292 * shortCubed
+        let greenLinear = -1.2684380046 * longCubed + 2.6097574011 * mediumCubed - 0.3413193965 * shortCubed
+        let blueLinear = -0.0041960863 * longCubed - 0.7034186147 * mediumCubed + 1.7076147010 * shortCubed
 
         func applyGamma(_ value: CGFloat) -> CGFloat {
             return value <= 0.0031308 ? 12.92 * value : 1.055 * pow(value, 1.0 / 2.4) - 0.055
